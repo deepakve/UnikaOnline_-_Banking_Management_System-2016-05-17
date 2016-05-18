c585a43b960c108667e578f64f7b24ef1825ec1b
deepakve <deepakdev88@gmail.com>
2016-05-17 23:29:08 -0600

    Create CustomerAccountsDAO.java

---
+++ src/com/unika/admin/CustomerAccountsDAO.java
@@ -0,0 +1,8 @@
+package com.unika.admin.accounts;
+
+import com.unika.hibernatemapping.CustomerAccounts;
+
+public interface CustomerAccountsDAO {
+
+	public void saveCustomerAccounts(CustomerAccounts ca);
+}
